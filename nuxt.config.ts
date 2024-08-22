8112a0539e674730fddd1b88c8550e1f14f778c0
PanPoke <kaiten4tei9yo5@gmail.com>
2024-08-22 15:39:10 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  // ページ設定を確認します
   pages: true, // デフォルト設定です
+  target: 'static', // 静的サイトとして生成
+  generate: {
+    fallback: '404.html', // 404ページとして使用されるファイル
+  },
+  app: {
+    baseURL: '/pokeooo/', // リポジトリ名に置き換えてください
+  },
 })
