563b40d235febe4df285e1e724ca3ea20abd5763
PanPoke <kaiten4tei9yo5@gmail.com>
2024-08-22 17:41:03 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,9 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/pokeooo/', // リポジトリ名に置き換えてください
   },
+  // 以下は GitHub Pages にデプロイする際に必要な設定です
+  build: {
+    // 出力ディレクトリを指定する
+    outDir: 'dist', // デフォルトの出力ディレクトリは `.nuxt` ですが、GitHub Pages に合わせて `dist` に変更
+  },
 })
